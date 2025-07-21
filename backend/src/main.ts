3e175ad1fa635f546227dcec5fef5d98be70f28a
Art Van der Vennet <art.van.der.vennet.be@gmail.com>
2025-07-21 19:44:26 +0200

    added database config file and swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,26 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { MikroORM } from '@mikro-orm/mysql'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT ?? 3000)
-  await MikroORM.init({
-    entities: ['./dist/app/**/*.entity.js'],
-    entitiesTs: ['./src/app/**/*.entity.ts'],
-    dbName: 'my-db-name',
-  })
+  const appOptions = { cors: true }
+  const app = await NestFactory.create(AppModule, appOptions)
+  app.setGlobalPrefix('api')
+
+  const config = new DocumentBuilder()
+    .setTitle('RabotPD')
+    .setDescription('The RabotPD API description')
+    .setVersion('1.0')
+    .addTag('api')
+    .addBearerAuth()
+    .build()
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('/docs', app, documentFactory)
+  SwaggerModule.setup('/docs-json', app, documentFactory)
+
+  await app.listen(3000)
 }
 
-bootstrap()
+bootstrap().catch((err) => {
+  console.log(err)
+})
