74972eea83e2c16641fd7067bc8de2962e345933
Art Van der Vennet <art.van.der.vennet.be@gmail.com>
2025-08-11 21:28:01 +0200

    added drinktype seeder

--- backend/src/drinktype/entities/drinktype.entity.ts
+++ backend/src/drinktype/entities/drinktype.entity.ts
@@ -1,1 +1,16 @@
-export class Drinktype {}
+import { Entity, PrimaryKey, Property } from '@mikro-orm/core'
+
+@Entity()
+export class Drinktype {
+  @PrimaryKey()
+  id!: number
+
+  @Property()
+  name: string
+
+  @Property()
+  default_price: number
+
+  @Property()
+  default_size: number
+}
